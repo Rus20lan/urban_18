527152ca63cece7e2c862bc41910e1b5b17dbb3f
Ruslan <ruslas@yandex.ru>
2024-12-02 23:55:54 +0300

    mod index.html - src='./src/main.tsx', vite.config.ts - base:'/urban_18'

--- src/router.tsx
+++ src/router.tsx
@@ -14,7 +14,7 @@ import Review from './components/pages/review/Review';
 export const router = createBrowserRouter(
   [
     {
-      path: '/urban_18/',
+      path: 'urban_18',
       element: <MainLayout />,
       children: [
         { index: true, element: <Home /> },

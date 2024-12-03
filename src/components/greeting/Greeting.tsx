79e2256856062ddcf886f78dbc38caf7636a9e5a
Ruslan <ruslas@yandex.ru>
2024-12-03 23:37:33 +0300

    mod greeting

--- src/components/greeting/Greeting.tsx
+++ src/components/greeting/Greeting.tsx
@@ -16,9 +16,6 @@ const Greeting = () => {
     <div className="display_greeting">
       <div className={style.container}>
         <img src={animBeans} alt="Jelly Belly Wiki Logo"></img>
-        {/* <div className={style.img_wrapper}>
-          
-        </div> */}
         <div className={style.text_wrapper}>
           <h1>Welcome to the World of Jelly Belly:</h1>
           <h3>A Rainbow of Flavors Awaits!</h3>
